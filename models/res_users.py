39646533530887cfbd094dc17ebfe8100539cfc0
Marc Aubry <mau@subteno.com>
2021-10-14 17:46:30 +0200

    [FIX] cannot open my profile, right issue in mailmultidomain

--- models/res_users.py
+++ models/res_users.py
@@ -17,3 +17,20 @@ class ResUsers(models.Model):
         comodel_name='ir.mail_server',
         help='The server mail used for the user if the configuration is split server mail by user',
     )
+
+    def __init__(self, pool, cr):
+        """ Override of __init__ to add access rights.
+            Access rights are disabled by default, but allowed
+            on some specific fields defined in self.SELF_{READ/WRITE}ABLE_FIELDS.
+        """
+
+        sale_stock_writeable_fields = [
+            'mail_user_alias_ids',
+            'server_mail_id',
+        ]
+
+        init_res = super().__init__(pool, cr)
+        # duplicate list to avoid modifying the original reference
+        type(self).SELF_READABLE_FIELDS = type(self).SELF_READABLE_FIELDS + sale_stock_writeable_fields
+        type(self).SELF_WRITEABLE_FIELDS = type(self).SELF_WRITEABLE_FIELDS + sale_stock_writeable_fields
+        return init_res
